9b136a51d303b83be93eb7bf510255627a488324
bl815v <jebedoyal@udistrital.edu.co>
2025-03-21 11:27:51 -0500

    Testing verification

--- Merge-vs-bubble/main.py
+++ Merge-vs-bubble/main.py
@@ -3,8 +3,7 @@ from controllers.random_number import RandomNumberController # pylint: disable=i
 from controllers.sort import BubbleSortController, MergeSortController # pylint: disable=import-error
 
 if __name__ == "__main__":
-    # TODO: Add a counter to count the number of comparisons and swaps for each algorithm.
+
     RandomNumberController.generate_and_save()
     BubbleSortController.sort_and_save()
     MergeSortController.sort_and_save()
-    
