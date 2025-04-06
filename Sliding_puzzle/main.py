ccf65f06a742bf439be38bba8a95eb3078c3a89b
Andrescpyo <cerdaspadillaandres@gmail.com>
2025-04-06 00:46:12 -0500

    testing solutions

--- Sliding_puzzle/main.py
+++ Sliding_puzzle/main.py
@@ -1,6 +1,12 @@
-
 from controllers.puzzle import PuzzleController
 
 if __name__ == "__main__":
-    PUZZLE = "123456789ABCDFE0"
+    PUZZLE = "123457689ABCDFE0"
     solution = PuzzleController.solve_puzzle(PUZZLE)
+    if solution is not None:
+        if isinstance(solution, str):
+            print(f"Solución encontrada")  # Mostrar mensajes como "El puzzle no tiene solución." o "Búsqueda terminada por límite..."
+        else:
+            print(f"Solución no encontrada: {solution}")
+    else:
+        print("No se encontró solución.")
